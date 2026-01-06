cf12e27a76294a981c52fbcb707bc492dec3fc6f
elpid <elpid@Elpida>
2026-01-06 23:19:09 +0200

    update

--- src/main/java/com/example/demo/Citizen.java
+++ src/main/java/com/example/demo/Citizen.java
@@ -13,23 +13,32 @@ public class Citizen {
 	private String email; 
 	private String password; 
 	
-	@OneToOne(mappedBy="citizen", cascade = CascadeType.ALL)
+	@OneToOne(mappedBy="citizen")
 	private Cart cart;
 	
 	public Citizen() {}
 	
 	public Citizen(int a, String fn, String sn, String e, String p) {
-		afm = a; 
-		firstName = fn; 
-		surName = sn; 
-		email = e; 
-		password = p; 
-	} 
-	
-	public int getAfm() {return afm;} 
-	public String getFirstName() {return firstName;} 
-	public String getSurName() {return surName;} 
-	public String getEmail() {return email;} 
-	public String getPassword() {return password;} 
-	
+        this.afm = a; 
+        this.firstName = fn; 
+        this.surName = sn; 
+        this.email = e; 
+        this.password = p; 
+    } 
+
+    // Getters
+    public int getAfm() {return afm;} 
+    public String getFirstName() {return firstName;} 
+    public String getSurName() {return surName;} 
+    public String getEmail() {return email;} 
+    public String getPassword() {return password;}
+    public Cart getCart() {return cart;}
+
+    // Setters 
+    public void setAfm(int afm) {this.afm = afm;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setSurName(String surName) {this.surName = surName;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPassword(String password) {this.password = password;}
+    public void setCart(Cart cart) {this.cart = cart;}
 }
