fa2cc4d4f5bf37f175b5f228fa952ecdad6e02ed
ioannisargyrakis <yiannis.argyrakis@gmail.com>
2026-01-06 12:24:19 +0200

    Added Service and Product

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	private String type;
+	
+	private String brand;
+	private double price;
+	private String description;
+	private int numberOfProducts;
+	
+	/*public Product(String type, String brand, double price, String description, int numberOfProducts, Store store) {
+		super();
+		this.type = type;
+		this.brand = brand;
+		this.price = price;
+		this.description = description;
+		this.numberOfProducts = numberOfProducts;
+		//Store αρχικοποίηση
+	}*/
+	
+	public String getBrand() {return brand;} 
+	public double getPrice() {return price;} 
+	public String getDescription() {return description;} 
+	public int getNumberOfProducts() {return numberOfProducts;} 
+	
+	
+}
