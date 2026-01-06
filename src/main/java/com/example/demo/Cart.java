0bc26a4f91811284cb9596dbb517d25abb7b288d
elpid <elpid@Elpida>
2026-01-06 22:59:13 +0200

    update

--- src/main/java/com/example/demo/Cart.java
+++ src/main/java/com/example/demo/Cart.java
@@ -1,28 +1,5 @@
 package com.example.demo;
 
-import java.util.*;
-import javax.persistence.*;
-
-@Entity
 public class Cart {
-	private int price; 
-	
-	@OneToOne
-    @JoinColumn(name = "citizen_afm")
-    private Citizen citizen;
-
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
-    private List<CartItem> items;
-
-	public Cart() {}
-	
-	public Cart(int p) {
-		price = p; 
-	} 
-	
-	public int getPrice() {return price;} 
 
 }
-
-	
-	
