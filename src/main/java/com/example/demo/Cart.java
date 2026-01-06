28d3ec92ce4cbae4fc8e762dd5c470d44a59d907
elpid <elpid@Elpida>
2026-01-06 23:44:33 +0200

    update

--- src/main/java/com/example/demo/Cart.java
+++ src/main/java/com/example/demo/Cart.java
@@ -1,5 +1,65 @@
 package com.example.demo;
 
+import java.util.Set;
+import javax.persistence.*;
+
+@Entity 
 public class Cart {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne
+	@JoinColumn(name = "citizen")
+	private Citizen citizen;
+	
+	@OneToMany(mappedBy="cart")
+	private Set<Product> products;
+	
+	private int total_price;
+	
+    public Cart() {}
+	
+	public Cart(Citizen citizen, int total_price) {
+		this.citizen = citizen;
+        this.total_price = total_price;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Citizen getCitizen() {
+		return citizen;
+	}
+
+	public void setCitizen(Citizen citizen) {
+		this.citizen = citizen;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	public int getTotal_price() {
+		return total_price;
+	}
+
+	public void setTotal_price(int total_price) {
+		this.total_price = total_price;
+	}
 
 }
+
+
+
+
--- src/main/java/com/example/demo/Citizen.java
+++ src/main/java/com/example/demo/Citizen.java
@@ -24,21 +24,54 @@ public class Citizen {
         this.surName = sn; 
         this.email = e; 
         this.password = p; 
-    } 
+    }
 
-    // Getters
-    public int getAfm() {return afm;} 
-    public String getFirstName() {return firstName;} 
-    public String getSurName() {return surName;} 
-    public String getEmail() {return email;} 
-    public String getPassword() {return password;}
-    public Cart getCart() {return cart;}
+	public int getAfm() {
+		return afm;
+	}
 
-    // Setters 
-    public void setAfm(int afm) {this.afm = afm;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public void setSurName(String surName) {this.surName = surName;}
-    public void setEmail(String email) {this.email = email;}
-    public void setPassword(String password) {this.password = password;}
-    public void setCart(Cart cart) {this.cart = cart;}
+	public void setAfm(int afm) {
+		this.afm = afm;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	} 
+
 }
