ce2e989372f0f5ae55fa9670f09986c69df8f882
ioannisargyrakis <yiannis.argyrakis@gmail.com>
2026-01-08 22:44:25 +0200

    Configuration first setup

---
+++ src/main/java/com/example/demo/ServiceConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfiguration implements CommandLineRunner {
+
+	@Autowired
+	private ServiceConfiguration sc;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	//Add products//
+	}
+
+}
