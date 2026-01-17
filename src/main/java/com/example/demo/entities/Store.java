e3dc8f9e0b8d9da435143bfd1ab177e2f836c08e
ioannisargyrakis <yiannis.argyrakis@gmail.com>
2026-01-17 17:46:11 +0200

    One to Many Store Product

--- src/main/java/com/example/demo/entities/Store.java
+++ src/main/java/com/example/demo/entities/Store.java
@@ -1,5 +1,7 @@
 package com.example.demo.entities;
 
+import java.util.List;
+
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -7,77 +9,74 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Store {
 	
-@Id
-   private int afm;
+	@Id
+	private int afm;
 
-   private String shopName; 
-   private String owner;   
-   private String password;
+	private String shopName; 
+	private String owner;   
+	private String password;
    
-   @OneToOne(mappedBy = "store", cascade = CascadeType.ALL)
-   @JsonIgnore // Αυτό θα σταματήσει τον κύκλο στο Swagger/JSON
-   private Product product;
+    @OneToOne(mappedBy = "store", cascade = CascadeType.ALL)
+    @JsonIgnore // Αυτό θα σταματήσει τον κύκλο στο Swagger/JSON
+    private List<Product> product;
    
-   public Store() {}
+    public Store() {}
     
-   public Store(int afm, String shopName, String owner, String password) {
-		this.afm = afm;
+    public Store(int afm, String shopName, String owner, String password) {
+	 	this.afm = afm;
 		this.shopName = shopName;
 		this.owner = owner;
 		this.password = password;
-   }
+    }
 
 
-   public int getAfm() {
-	return afm;
-   }
+    public int getAfm() {
+    	return afm;
+    }
 
 
-   public void setAfm(int afm) {
-	this.afm = afm;
-   }
+    public void setAfm(int afm) {
+	 this.afm = afm;
+    }
+ 
+
+    public String getShopName() {
+	 return shopName;
+    }
 
 
-   public String getShopName() {
-	return shopName;
-   }
+    public void setShopName(String shopName) {
+	 this.shopName = shopName;
+    }
 
 
-   public void setShopName(String shopName) {
-	this.shopName = shopName;
-   }
+    public String getOwner() {
+	 return owner;
+    }
 
 
-   public String getOwner() {
-	return owner;
-   }
+    public void setOwner(String owner) {
+	 this.owner = owner;
+    }
 
 
-   public void setOwner(String owner) {
-	this.owner = owner;
-   }
+    public String getPassword() {
+	 return password;
+    }
 
 
-   public String getPassword() {
-	return password;
-   }
+    public void setPassword(String password) {
+	 this.password = password;
+    }
 
+	public List<Product> getProduct() {
+		return product;
+	}
 
-   public void setPassword(String password) {
-	this.password = password;
-   }
-
-
-   public Product getProduct() {
-	return product;
-   }
-
-
-   public void setProduct(Product product) {
-	this.product = product;
-   }
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
    
-  
 }
    
 
