4292db3a140dac011396552eb9a7526ff94cc2dc
elpid <elpid@Elpida>
2026-02-01 13:43:18 +0200

    update

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -12,8 +12,8 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 
 @Query(
 	   "SELECT p FROM Product p " +
-       "WHERE (:type IS NULL OR p.type LIKE %:type%) " + //ο χρήστης μπορεί να βρεί το προϊόν ακόμη και αν δεν το γράψει ακριβώς όπως πρέπει(π.χ.Lap αντί για Laptop)
-	   "AND (:brand IS NULL OR p.brand LIKE %:brand%) " +
+       "WHERE (:type IS NULL OR LOWER(p.type) LIKE LOWER(CONCAT('%', :type, '%'))) " + //ο χρήστης μπορεί να βρεί το προϊόν ακόμη και αν δεν το γράψει ακριβώς όπως πρέπει(π.χ.Lap αντί για Laptop)
+       "AND (:brand IS NULL OR LOWER(p.brand) LIKE LOWER(CONCAT('%', :brand, '%'))) " +
 	   "AND (:minPrice IS NULL OR p.price >= :minPrice) " +
        "AND (:maxPrice IS NULL OR p.price <= :maxPrice)")
         List<Product> search(@Param("type") String type, @Param("brand") String brand, @Param("minPrice") Double minPrice, @Param("maxPrice") Double maxPrice);
